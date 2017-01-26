c5ac53ecbf81972719348112333e3f9723583b44
andie5 <andieasa@googlemail.com>
2017-01-26 22:01:09 +0000

    completed warmup tests

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -7,8 +7,6 @@ describe Warmup do
 
   describe 'gets_shout' do
     it 'returns the input uppercase' do
-      # allow(user).to receive(:name)
-
       allow(warmup).to receive(:gets).and_return("hello")
       expect(warmup.gets_shout).to eq("HELLO")
     end
@@ -17,6 +15,16 @@ describe Warmup do
   describe 'triple_size' do
     it 'triples the size of the existing element' do
       expect(warmup.triple_size(arr)).to eq(6)
+    end
+  end
+
+  describe 'calls_some_methods' do
+    it 'returns a default string no matter what the input is' do
+      expect(warmup.calls_some_methods("Welcome")).to eq("I am unrelated")
+    end
+
+    it 'raises an error if an empty string is supplied' do
+      expect{ warmup.calls_some_methods("")}.to raise_error("Hey, give me a string!")
     end
   end
 end
